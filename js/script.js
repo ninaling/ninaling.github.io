49a2de9b6095edcdfe380d835fa21fdc08211426
thetoothless <ninaling@ucla.edu>
2016-09-10 23:57:59 +0800

    setup ascensor and jquery ui

--- js/script.js
+++ js/script.js
@@ -1,4 +1,10 @@
-var myApp=angular.module("myModule", []);
-myApp.controller("myController", function($scope){
-  
+$(document).ready(function(){
+  $('#ascensor').ascensor({
+    childType: "section",
+    time: 500,
+    windowsOn: 0,
+    direction: 'y',
+    easing: "easeInOutQuart"
+  });
+  // var ascensorInstance = ascensor.data('ascensor');
 });

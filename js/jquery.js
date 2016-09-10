46524407b70300130a9b44f4a550bf4a5c452a01
thetoothless <ninaling@ucla.edu>
2016-09-10 23:17:41 +0800

    starting over yay

--- js/jquery.js
+++ js/jquery.js
@@ -1,31 +1,3 @@
-$(function() {
-    $( "#selectable" ).selectable();
-});
-
 $(document).ready(function(){
-    $("#remove_under_construction").click(function(){
-        $("#under_construction").hide();
-    });
-    $("#top_option").click(function(){
-        $("html, body").animate({
-            scrollTop: $("#landing").offset().top
-        }, 500);
-    });
-    $("#about_option").click(function(){
-        $("html, body").animate({
-            scrollTop: $("#about").offset().top-20
-        }, 500);
-    });
-    $("#education_option").click(function(){
-        $("html, body").animate({
-            scrollTop: $("#education").offset().top-20
-        }, 500);
-    });
-    $(".navigator_option").hover(
-        function() {
-            $(this).css("opacity", 1);
-        }, function() {
-            $(this).css("opacity", 0.5);
-        }
-    );
+
 });

41a28ca69721921a40db0165108ed2e857874fd4
thetoothless <ninaling@ucla.edu>
2016-09-15 20:52:45 +0800

    svg's animated :3

--- js/script.js
+++ js/script.js
@@ -1,10 +1,36 @@
 $(document).ready(function(){
-  $('#ascensor').ascensor({
+  var ascensor = $('#ascensor').ascensor({
     childType: "section",
+    ascensorFloorName: ['left-brain','right-brain'],
     time: 500,
     windowsOn: 0,
     direction: 'x',
     easing: "easeInOutQuart"
   });
+
+  var leftBrain = new Vivus('left-brain-img', {type: 'delayed', duration: 220, delay: 180, start: 'autostart'});
+  var leftBrainNarrow = new Vivus('left-brain-img-narrow', {type: 'delayed', duration: 220, delay: 180, start: 'autostart'});
+  var rightBrain = new Vivus('right-brain-img', {type: 'delayed', duration: 220, delay: 180, start: 'autostart'});
+  var rightBrainNarrow = new Vivus('right-brain-img-narrow', {type: 'delayed', duration: 220, delay: 180, start: 'autostart'});
+
+  var graphics = [leftBrain, leftBrainNarrow, rightBrain, rightBrainNarrow];
+
   // var ascensorInstance = ascensor.data('ascensor');
+  ascensor.on("scrollStart", function(e, floor){
+    for (var i = 0; i < graphics.length; i++){
+      var graphic = graphics[i];
+      if (graphic.map) {
+        graphic.stop().reset();
+      }
+    }
+  });
+
+  ascensor.on("scrollEnd", function(e, floor){
+    for (var i = 0; i < graphics.length; i++){
+      var graphic = graphics[i];
+      if (graphic.map) {
+        graphic.play();
+      }
+    }
+  });
 });
